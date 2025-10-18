c937da53d30ccecfc594c740e211b496382b340a
santiagopalacios07 <tpalacioxd@gmail.com>
2025-10-17 20:19:19 -0500

    Agregar código inicial de cálculo de notas

---
+++ Estructuras_De_Control/Estructuras_De_Control/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace estructurasdecontrol_3147912
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su primer nota: .");
+            float nota1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su segunda nota: .");
+            float nota2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su tercera nota: .");
+            float nota3 = float.Parse(Console.ReadLine());
+
+            float promedio = (nota1 * 0.2f) + (nota2 * 0.3f) + (nota3 * 0.5f);
+            if (promedio >=3)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                Console.WriteLine("No aprobado");                                                                                                                          
+            }
+        }
+    }
+}
+
+ 
+
+
+
